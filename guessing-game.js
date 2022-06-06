e56ba1411dcfd5ed3328e8cc2fb61a13f5ce13f1
Sukhdev Gill <26435947+sukhdevgill@users.noreply.github.com>
2022-06-06 01:51:33 +0100

    Import readline module

--- guessing-game.js
+++ guessing-game.js
@@ -1,3 +1,19 @@
+// Import the readline module into our file
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
+// Create an interface where we can talk to the user
+const rl = readline.createInterface({ input, output });
+
+// Ask user a question
+rl.question('What do you think of Node.js? ', (answer) => {
+  // Log their answer
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+  // Close the interface
+  rl.close();
+});
+
+
 let secretNumber = 42;
 
 const checkGuess = (num) => {
